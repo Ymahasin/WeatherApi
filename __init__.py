aa1756fac1ec4535876c580b61c762ebf535eb54
Ying <fly.ying@gmail.com>
2018-07-09 20:37:19 -0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+This file is to house my private detail for WeatherAPI
+
+account_sid = 'AC1db73e888f1fb962ec453b0f643337c6'
+auth_token = 'f8ad6cb11cece89c7f3c059504f9c2b9'
+#14086179528 Twilio phone number (for send from)
+
+callFrom = "14086179528"
+callTo = "14086246828"
+
+
+"""
